13291fbc739c4babad234d53058a9523ab370c9a
Simzahn <simon.hochenberger@chello.at>
2023-08-10 06:51:11 +0200

    Initial commit + Movie class
    Added the movie class, which can hold a (maybe) loaded movie.

---
+++ src/main/java/me/simzahn/spigotmovieplayer/Main.java
@@ -0,0 +1,17 @@
+package me.simzahn.spigotmovieplayer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SpigotMoviePlayer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/me/simzahn/spigotmovieplayer/Movie.java
@@ -0,0 +1,2 @@
+package me.simzahn.spigotmovieplayer;public class Movie {
+}
